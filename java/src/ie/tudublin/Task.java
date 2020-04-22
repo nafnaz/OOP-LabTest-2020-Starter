367d303471e1aa239a75552776da21cc3d2d342e
Nafisa Naz <C16715071@mytudublin.ie>
2020-04-22 18:20:55 +0100

    Task model

---
+++ java/src/ie/tudublin/Task.java
@@ -0,0 +1,32 @@
+package ie.tudublin;
+
+public class Task {
+    private String taskName;
+    private int start;
+    private int end;
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public void setStart(int start) {
+        this.start = start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+
+    public void setEnd(int end) {
+        this.end = end;
+    }
+
+}
